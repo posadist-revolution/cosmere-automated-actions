d2ef563d5d72c7ebde1a375530efc7c1b7bd282b
Lena Hunicke-Smith <ihunickesmith@gmail.com>
2026-02-03 20:47:57 -0700

    Moved helpers to typescript

---
+++ src/utils/helpers.ts
@@ -0,0 +1,41 @@
+import { CosmereItem } from "@src/declarations/cosmere-rpg/documents/item";
+
+//Module Functions
+export function IsModuleActive(moduleId: string) {
+	return game.modules?.get(moduleId)?.active;
+}
+
+export function nameToId(str: string) {
+    return str.toLowerCase().split(' ').join('-');
+}
+
+//Actor Functions
+export function getFirstTarget(){
+    return game.user?.targets.first();
+}
+export function getAllTargets(){
+    return game.user?.targets
+}
+
+//Item Functions
+export async function activateAllItemEffects(item: CosmereItem){
+    const updates = item.effects.map(effect => ({ _id: effect.id, disabled: !effect.disabled }));
+    await item.updateEmbeddedDocuments("ActiveEffect", updates);
+}
+export async function giveActorItem(actor: CosmereActor, itemUUID: string){
+    const itemDocument = fromUuidSync(itemUUID);
+    if (!(itemDocument instanceof Item)) {
+        throw new Error(`UUID ${itemUUID} does not reference a valid Item`);
+    }
+    const item = await Item.create((itemDocument.toObject()), { parent: actor as unknown as Actor });
+    return item;
+}
+export function getFlags(item: CosmereItem){
+    const cosmereFlags = item.flags["cosmere-automated-actions"];
+    const worldFlags = item.flags["world"];
+    if(cosmereFlags){
+        return cosmereFlags;
+    } else {
+        return worldFlags;
+    }
+}
